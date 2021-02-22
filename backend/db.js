9f36a8d5c6e6b68d6f148f0a0da3587ba4b2c48d
Thomas Druart <thomas.druart.59@gmail.com>
2021-02-22 11:01:13 +0100

    dotenv installed and env sample added

---
+++ backend/db.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+const Pool = require("pg").Pool;
+
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT } = process.env;
+
+const pool = new Pool({
+  user: DB_USER,
+  password: DB_PASSWORD,
+  host: DB_HOST,
+  port: DB_PORT,
+  database: DB_NAME,
+});
