d2b18befd755496a0fb1d6e815576cefd98fd78f
Thomas Druart <thomas.druart.59@gmail.com>
2021-02-22 13:39:47 +0100

    all routes done and working

--- backend/index.js
+++ backend/index.js
@@ -17,20 +17,60 @@ app.post("/todos", async (req, res) => {
       "INSERT INTO todo (description) VALUES ($1) RETURNING *",
       [description]
     );
-    res.json(newTodo);
+    res.json(newTodo.rows[0]);
   } catch (err) {
     console.error(err.message);
   }
 });
 
 // get all todos
+app.get("/todos", async (req, res) => {
+  try {
+    const allTodos = await pool.query("SELECT * FROM todo");
+    res.json(allTodos.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
 // get a todo
+app.get("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await pool.query("SELECT * FROM todo WHERE id = $1", [id]);
+    res.json(todo.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
 // update a todo
+app.put("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { description } = req.body;
+    const updateTodo = await pool.query(
+      "UPDATE todo SET description = $1 WHERE id = $2",
+      [description, id]
+    );
+    res.json("todo updated!");
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
 // delete a todo
+app.delete("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteTodo = await pool.query("DELETE FROM todo WHERE id = $1", [id]);
+    res.json("todo deleted!");
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
+// 404 ERROR AND LISTEN
 app.use((req, res) => {
   const msg = `Page not found: ${req.url}`;
   console.warn(msg);
