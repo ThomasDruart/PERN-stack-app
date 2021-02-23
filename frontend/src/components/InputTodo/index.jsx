a2a89056f05b0f6d629a24a1d018a1c912339030
Thomas Druart <thomas.druart.59@gmail.com>
2021-02-23 10:50:18 +0100

    input component done with axios.post

---
+++ frontend/src/components/InputTodo/index.jsx
@@ -0,0 +1,36 @@
+import React, { Fragment, useState } from "react";
+import axios from "axios";
+
+const InputTodo = () => {
+  const [description, setDescription] = useState("test");
+
+  const onSubmitForm = async (e) => {
+    e.preventDefault();
+    try {
+      const body = { description };
+      const response = await axios.post("http://localhost:5000/todos", body);
+      console.log(response.data);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  return (
+    <Fragment>
+      <h1 className="text-center mt-5">PERN Todo List</h1>
+      <form className="d-flex mt-5" onSubmit={onSubmitForm}>
+        <input
+          type="text"
+          className="form-control"
+          value={description}
+          onChange={(e) => {
+            setDescription(e.target.value);
+          }}
+        />
+        <button className="btn btn-success">Add</button>
+      </form>
+    </Fragment>
+  );
+};
+
+export default InputTodo;
